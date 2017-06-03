059c275a7f2ae2f2830af7f08e145c3cfc4ccee0
MINSOO,NAM <ukun3147@naver.com>
2017-06-04 03:58:55 +0900

    Todo CRUD 기본 기능 완료.

--- fe/js/app.js
+++ fe/js/app.js
@@ -6,57 +6,36 @@
     const TYPE_ALL = 'all';
     const TYPE_ACTIVE = 'active';
     const TYPE_COMPLETED = 'completed'
-    var todoList = $('.todo-list');
-    var todoCount = $('.todo-count');
-
+    var $todoList = $('.todo-list');
+    var $todoCount = $('.todo-count');
 
     $(function() {
         setTodoList(TYPE_ALL);
     });
 
-
-
-
-
-
-
-
+    $('.footer').on('click', '.clear-completed', function() {
+        deleteCompleted();
+    });
 
     $('.main').on('click', '.destroy', function() {
-        //할 일 삭제
-        //디비에 삭제
+        var id = $(this).parents('li').children('div').children('input').val();
+        var liIndex = $(this).parents('li').index();
 
-
-        alert("클릭했어요");
+        deleteTodo(id, liIndex);
     });
-
-    $('.footer').on('click', '.clear-completed', function() {
-        //완료된 할 일 삭제
-        //디비에 삭제
-
-        alert("클릭했어요");
-    });
-
-
 
     $('.main').on('click', '.toggle', function() {
-        //할 일 완료/미완료를 왔다갔다거리게
-        //디비 수정
-
+        var $toggle = $(this);
         var id = this.value;
-        var todo = $(this).parents('view').find('label').html;
-        var completed = this.checked; //바뀔 값
+        var completed = this.checked;
 
         var todoData = {
             'id': id,
             'completed': completed
         };
-//updateTodo(todoData);
-//$(this).parents('li').addClass('completed');
-        alert(todo);
+
+        updateTodo(todoData, $toggle);
     });
-
-
 
     $('.new-todo').on('keydown', function(event) {
         if (event.which == 13) {
@@ -79,16 +58,52 @@
         }
     });
 
-
     $('.filters li').on('click', function(event) {
         setTodoList(event.target.id);
         event.stopPropagation();
-        //li의 색을 바꿔주는 기능까지?
     });
 
 
+    function deleteCompleted() {
+        $.ajax({
+            method: 'DELETE',
+            url: '/api/todos/completed/',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(data) {
+                var completedList = $todoList.children('li.completed')
 
-    function updateTodo(todoData) {
+                completedList.remove();
+
+                var count = $todoCount.html().split(' ');
+                setTodoCount(count[0] - completedList.length)
+            },
+            error: function() {
+                alert('failed delete completed');
+            }
+        });
+    }
+
+    function deleteTodo(id, liIndex) {
+        $.ajax({
+            method: 'DELETE',
+            url: '/api/todos/' + id,
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(data) {
+                $todoList.children('li').eq(liIndex).remove()
+
+                var count = $todoCount.html().split(' ');
+                setTodoCount(--count[0])
+            },
+            error: function() {
+                alert('failed delete todo');
+            }
+        });
+    }
+
+
+    function updateTodo(todoData, $toggle) {
         $.ajax({
             method: 'PUT',
             url: '/api/todos/' + todoData.id,
@@ -96,21 +111,22 @@
             dataType: 'json',
             data: JSON.stringify(todoData),
             success: function(data) {
-                //alert('completed add todo');
-
-//li class 변경
-$(this).parents('li').addClass('completed');
-
+                if (todoData.completed) {
+                    $toggle.parents('li').addClass('completed');
+                } else if (!todoData.completed) {
+                    $toggle.parents('li').removeClass('completed');
+                }
             },
             error: function() {
-
-              //체크 원래대로..... 그냥 리스트 다시 부르까...?
-                alert('failed add todo');
+                if (todoData.completed) {
+                    $toggle.prop('checked', false);
+                } else if (!todoData.completed) {
+                    $toggle.prop('checked', true);
+                }
+                alert('failed update todo');
             }
-
         });
     }
-
 
     function addTodo(todoData) {
         $.ajax({
@@ -119,21 +135,17 @@ $(this).parents('li').addClass('completed');
             contentType: 'application/json',
             dataType: 'json',
             data: JSON.stringify(todoData),
-            success: function(data) {
-                //alert('completed add todo');
-                todoList.prepend(getLiString(todoData));
+            success: function(newTodoData) {
+                $todoList.prepend(getLiString(newTodoData));
 
-                var count = todoCount.html().split(' ');
+                var count = $todoCount.html().split(' ');
                 setTodoCount(++count[0])
-
             },
             error: function() {
                 alert('failed add todo');
             }
-
         });
     }
-
 
     function setTodoList(type) {
         $.ajax({
@@ -145,7 +157,7 @@ $(this).parents('li').addClass('completed');
                 var str = '';
                 var count = 0;
 
-                todoList.empty();
+                $todoList.empty();
                 for (var i in data) {
                     if (type == TYPE_ALL) {
                         str += getLiString(data[i]);
@@ -158,25 +170,29 @@ $(this).parents('li').addClass('completed');
                         count++;
                     }
                 }
-                todoList.append(str);
+                $todoList.append(str);
 
                 setTodoCount(count);
-
-
             },
             error: function() {
-
+                alert('failed get todo list');
             }
         });
     }
 
+
     function getLiString(data) {
-        return `<li ` + (data.completed == IS_COMPLETED ? `class="completed"` : ``) + `><div class="view"><input class="toggle" type="checkbox"` + (data.completed == IS_COMPLETED ? ` checked` : ``)+` value="` +data.id+ `"><label>` + data.todo + `</label><button class="destroy"></button></div></li>`
+        return `<li ` + (data.completed == IS_COMPLETED ? `class="completed">` : `>`) +
+            `<div class="view">` +
+            `<input class="toggle" type="checkbox"` + (data.completed == IS_COMPLETED ? ` checked` : ``) + ` value="` + data.id + `">` +
+            `<label>` + data.todo + `</label>` +
+            `<button class="destroy"></button>` +
+            `</div>` +
+            `</li>`
     }
 
     function setTodoCount(count) {
-        todoCount.html(count + ' item left');
+        $todoCount.html(count + ' item left');
     }
-
 
 })(window);
