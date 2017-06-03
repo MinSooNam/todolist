a957af5676229adf2e32985e5b21b3d660aac8e4
MINSOO,NAM <ukun3147@naver.com>
2017-06-04 01:36:40 +0900

    sql 코드 변경 - 최신 시간순으로 정령 코드 삽입

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,12 +1,12 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
-	public static final String SELECT_ALL = "SELECT * FROM TODO";	
-	public static final String SELECT_BY_ID = "SELECT * FROM TODO WHERE id= :id";
-	public static final String SELECT_BY_COMPLETED = "SELECT * FROM TODO WHERE completed= :completed";
+	public static final String SELECT_ALL = "SELECT * FROM TODO ORDER BY date DESC";	
+	public static final String SELECT_BY_ID = "SELECT * FROM TODO WHERE id= :id  ORDER BY date DESC";
+	public static final String SELECT_BY_COMPLETED = "SELECT * FROM TODO WHERE completed= :completed  ORDER BY date DESC";
+	
 	public static final String UPDATE =
 			"UPDATE TODO SET\n"
-			+ "todo = :todo,\n"
 			+ "completed = :completed\n"
 			+ "WHERE id = :id";
 	public static final String DELETE_BY_ID = "DELETE FROM TODO WHERE id= :id";
