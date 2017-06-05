c4e9af850724bfc6f05cbd1f257631b57c19d49a
MINSOO,NAM <ukun3147@naver.com>
2017-06-05 15:07:14 +0900

    FrontEnd index.html, app.js 주석 추가 - 함수 순서 변경

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,40 +1,20 @@
 (function(window) {
     'use strict';
-
     // Your starting point. Enjoy the ride!
+
     const IS_COMPLETED = true;
     const TYPE_ALL = 'all';
     const TYPE_ACTIVE = 'active';
     const TYPE_COMPLETED = 'completed'
+
     var $todoList = $('.todo-list');
     var $todoCount = $('.todo-count');
 
+    /*
+        register events
+    */
     $(function() {
         setTodoList(TYPE_ALL);
-    });
-
-    $('.footer').on('click', '.clear-completed', function() {
-        deleteCompleted();
-    });
-
-    $('.main').on('click', '.destroy', function() {
-        var id = $(this).parents('li').children('div').children('input').val();
-        var liIndex = $(this).parents('li').index();
-
-        deleteTodo(id, liIndex);
-    });
-
-    $('.main').on('click', '.toggle', function() {
-        var $toggle = $(this);
-        var id = this.value;
-        var completed = this.checked;
-
-        var todoData = {
-            'id': id,
-            'completed': completed
-        };
-
-        updateTodo(todoData, $toggle);
     });
 
     $('.new-todo').on('keydown', function(event) {
@@ -58,50 +38,91 @@
         }
     });
 
+    $('.main').on('click', '.toggle', function() {
+        var $toggle = $(this);
+        var id = this.value;
+        var completed = this.checked;
+
+        var todoData = {
+            'id': id,
+            'completed': completed
+        };
+
+        updateTodo(todoData, $toggle);
+    });
+
+    $('.main').on('click', '.destroy', function() {
+        var id = $(this).parents('li').children('div').children('input').val();
+        var liIndex = $(this).parents('li').index();
+
+        deleteTodo(id, liIndex);
+    });
+
+    $('.footer').on('click', '.clear-completed', function() {
+        deleteCompleted();
+    });
+
     $('.filters li').on('click', function(event) {
         setTodoList(event.target.id);
         event.stopPropagation();
     });
 
 
-    function deleteCompleted() {
+    /*
+        Jquery ajax functions
+    */
+
+    function setTodoList(type) {
         $.ajax({
-            method: 'DELETE',
-            url: '/api/todos/completed/',
+            method: 'GET',
+            url: '/api/todos',
             contentType: 'application/json',
             dataType: 'json',
             success: function(data) {
-                var completedList = $todoList.children('li.completed')
+                var str = '';
+                var count = 0;
 
-                completedList.remove();
+                $todoList.empty();
+                for (var i in data) {
+                    if (type == TYPE_ALL) {
+                        str += getHtmlLi(data[i]);
+                        count++;
+                    } else if (type == TYPE_ACTIVE && data[i].completed != IS_COMPLETED) {
+                        str += getHtmlLi(data[i]);
+                        count++;
+                    } else if (type == TYPE_COMPLETED && data[i].completed == IS_COMPLETED) {
+                        str += getHtmlLi(data[i]);
+                        count++;
+                    }
+                }
+                $todoList.append(str);
 
-                var count = $todoCount.html().split(' ');
-                setTodoCount(count[0] - completedList.length)
+                setTodoCount(count);
             },
             error: function() {
-                alert('failed delete completed');
+                alert('failed get todo list');
             }
         });
     }
 
-    function deleteTodo(id, liIndex) {
+    function addTodo(todoData) {
         $.ajax({
-            method: 'DELETE',
-            url: '/api/todos/' + id,
+            method: 'POST',
+            url: '/api/todos',
             contentType: 'application/json',
             dataType: 'json',
-            success: function(data) {
-                $todoList.children('li').eq(liIndex).remove()
+            data: JSON.stringify(todoData),
+            success: function(newTodoData) {
+                $todoList.prepend(getHtmlLi(newTodoData));
 
                 var count = $todoCount.html().split(' ');
-                setTodoCount(--count[0])
+                setTodoCount(++count[0])
             },
             error: function() {
-                alert('failed delete todo');
+                alert('failed add todo');
             }
         });
     }
-
 
     function updateTodo(todoData, $toggle) {
         $.ajax({
@@ -128,60 +149,49 @@
         });
     }
 
-    function addTodo(todoData) {
+    function deleteTodo(id, liIndex) {
         $.ajax({
-            method: 'POST',
-            url: '/api/todos',
-            contentType: 'application/json',
-            dataType: 'json',
-            data: JSON.stringify(todoData),
-            success: function(newTodoData) {
-                $todoList.prepend(getLiString(newTodoData));
-
-                var count = $todoCount.html().split(' ');
-                setTodoCount(++count[0])
-            },
-            error: function() {
-                alert('failed add todo');
-            }
-        });
-    }
-
-    function setTodoList(type) {
-        $.ajax({
-            method: 'GET',
-            url: '/api/todos',
+            method: 'DELETE',
+            url: '/api/todos/' + id,
             contentType: 'application/json',
             dataType: 'json',
             success: function(data) {
-                var str = '';
-                var count = 0;
+                $todoList.children('li').eq(liIndex).remove()
 
-                $todoList.empty();
-                for (var i in data) {
-                    if (type == TYPE_ALL) {
-                        str += getLiString(data[i]);
-                        count++;
-                    } else if (type == TYPE_ACTIVE && data[i].completed != IS_COMPLETED) {
-                        str += getLiString(data[i]);
-                        count++;
-                    } else if (type == TYPE_COMPLETED && data[i].completed == IS_COMPLETED) {
-                        str += getLiString(data[i]);
-                        count++;
-                    }
-                }
-                $todoList.append(str);
-
-                setTodoCount(count);
+                var count = $todoCount.html().split(' ');
+                setTodoCount(--count[0])
             },
             error: function() {
-                alert('failed get todo list');
+                alert('failed delete todo');
+            }
+        });
+    }
+
+    function deleteCompleted() {
+        $.ajax({
+            method: 'DELETE',
+            url: '/api/todos/completed/',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function(data) {
+                var completedList = $todoList.children('li.completed')
+
+                completedList.remove();
+
+                var count = $todoCount.html().split(' ');
+                setTodoCount(count[0] - completedList.length)
+            },
+            error: function() {
+                alert('failed clear-completed');
             }
         });
     }
 
 
-    function getLiString(data) {
+    /*
+      Helper functions
+    */
+    function getHtmlLi(data) {
         return `<li ` + (data.completed == IS_COMPLETED ? `class="completed">` : `>`) +
             `<div class="view">` +
             `<input class="toggle" type="checkbox"` + (data.completed == IS_COMPLETED ? ` checked` : ``) + ` value="` + data.id + `">` +
